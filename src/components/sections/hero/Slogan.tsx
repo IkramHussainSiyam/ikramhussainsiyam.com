a2ae6dd4e2f559981d6a52eba8c8e804bac1169e
IkramHussainSiyam <ikramhussainsiyam@gmail.com>
2025-07-10 22:10:59 +0600

    chore: some small changes
    - changed profesional title font weight
    - changed themening

--- src/components/sections/hero/Slogan.tsx
+++ src/components/sections/hero/Slogan.tsx
@@ -1,6 +1,6 @@
 export default function Slogan() {
   return (
-    <h3 className="text-2xl sm:text-4xl xl:text-5xl font-semibold sm:font-medium capitalize fade-in-bck [--wait:0.7s]">
+    <h3 className="text-2xl sm:text-4xl xl:text-5xl font-medium capitalize fade-in-bck [--wait:0.7s]">
       frontend web developer
     </h3>
   );
